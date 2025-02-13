4b75a6befff7983e6066e51c4b13dfd332fe078c
Somdev Behera <64832169+somdev-hub@users.noreply.github.com>
2025-02-13 23:40:54 +0530

    configure Telegram bot for webhook handling and update server setup

--- server.js
+++ server.js
@@ -132,6 +132,11 @@ app.get("/api/insights", async (req, res) => {
   }
 });
 
+app.post(`/${process.env.TELEGRAM_BOT_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- telegram.js
+++ telegram.js
@@ -2,11 +2,17 @@ const TelegramBot = require("node-telegram-bot-api");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+// const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.setWebHook(
-  `https://form-status-check.onrender.com/${process.env.TELEGRAM_BOT_TOKEN}`
-);
+// bot.setWebHook(
+//   `https://form-status-check.onrender.com/${process.env.TELEGRAM_BOT_TOKEN}`
+// );
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+  webHook: {
+    port: process.env.PORT
+  }
+});
 
 // const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 

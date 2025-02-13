00465d1fb99ceaadd80e557f27e26c56c15bf64d
Somdev Behera <64832169+somdev-hub@users.noreply.github.com>
2025-02-13 20:17:48 +0530

    update Telegram bot webhook URL and adjust bot initialization

--- telegram.js
+++ telegram.js
@@ -2,12 +2,12 @@ const TelegramBot = require("node-telegram-bot-api");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
-// bot.setWebHook(
-//   `https://fetch-marks-production.up.railway.app/${process.env.TELEGRAM_BOT_TOKEN}`
-// );
+bot.setWebHook(
+  `https://form-status-check.onrender.com/${process.env.TELEGRAM_BOT_TOKEN}`
+);
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+// const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
 module.exports = bot;
